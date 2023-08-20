ba491223c623292dc92925134f2d8fefc1387d15
shobonbon55 <shoheitakahashi1208@gmail.com>
2023-08-20 15:26:00 +0900

    バリテーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one    :order
   has_one_attached :image
+
+  validates :item_name,   presence:true
+  validates :description, presence:true
+  validates :price,       presence:true, numericality: { in: 300..99999 }
+
 end
