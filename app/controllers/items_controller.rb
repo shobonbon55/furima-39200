dea6ff4b6a6fb61099832eacc19682828aebd84a
shobonbon55 <shoheitakahashi1208@gmail.com>
2023-08-26 20:21:28 +0900

    テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, except: [:index, :new, :create]
 
+  def index
+    @items = Item.all
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    
+    if @item.save
+      redirect_to root_path
+    else
+       render :new
+     end
+    end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :courier_fare_id, :send_area_id, :send_period_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    if user_signed_in?
+      @item = Item.find(params[:id])
+      unless current_user == @item.user
+        redirect_to action: :index
+      end
+    end
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name             {Faker::Name.initials(number: 40)}
+    description           {Faker::Lorem.characters(number: 10)}
+    category_id           {Faker::Number.between(from: 2, to: 5) }
+    condition_id          {Faker::Number.between(from: 2, to: 6) }
+    courier_fare_id       {Faker::Number.between(from: 2, to: 3) }
+    send_area_id          {Faker::Number.between(from: 2, to: 10) }
+    send_period_id        {Faker::Number.between(from: 2, to: 4) }
+    price                 {Faker::Number.between(from: 300, to: 99999) }
+    association :user 
+
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
+
+
