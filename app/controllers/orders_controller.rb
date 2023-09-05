d29eca701829fd63819eeddde691bd514ec0e0d9
shobonbon55 <shoheitakahashi1208@gmail.com>
2023-09-05 21:06:11 +0900

    手順2

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+   params.require(:order_address)
+  #  .permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
+  end
+
+
+
+end
