9fa98c565fa37dd29ebf660841c5485ac4317bd2
shobonbon55 <shoheitakahashi1208@gmail.com>
2023-09-17 10:45:09 +0900

    本番環境修正2

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,7 +1,7 @@
 class Condition < ActiveHash::Base
   self.data = [
       { id: 1, name: '---' },
-      { id: 2, name: '新商品・未使用' },
+      { id: 2, name: '新品・未使用' },
       { id: 3, name: '未使用に近い' },
       { id: 4, name: 'やや傷や汚れあり' },
       { id: 5, name: '目立った傷や汚れなし' },
